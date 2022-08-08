493fb04037d7aa67e2d68654af0f100dc6736dca
Yidu-xSkript <yidu95@gmail.com>
2022-08-08 11:33:03 +0300

    main.h modific

--- main.h
+++ main.h
@@ -6,6 +6,19 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+/**
+ * struct buffer_s - A new type defining a buffer struct.
+ * @buffer: A pointer to a character array.
+ * @start: A pointer to the start of buffer.
+ * @len: The length of the string stored in buffer.
+ */
+typedef struct buffer_s
+{
+	char *buffer;
+	char *start;
+	unsigned int len;
+} buffer_t;
+
 int _printf(const char *format, ...);
 
 #endif /* MAIN_H */
